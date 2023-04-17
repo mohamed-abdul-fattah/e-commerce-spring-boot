81e9520a8074d7e8dc2d23ecdc2c0cb5a4609eff
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2023-04-17 13:03:46 +0200

    Added checkstyle Java linter

--- src/main/java/eg/espace/ecommerce/ECommerceApplication.java
+++ src/main/java/eg/espace/ecommerce/ECommerceApplication.java
@@ -3,11 +3,20 @@ package eg.espace.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// CHECKSTYLE:OFF: AbbreviationAsWordInName
+/**
+ * This class is the main class of the application.
+ */
 @SpringBootApplication
 public class ECommerceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ECommerceApplication.class, args);
-    }
+  /**
+   * The main method of the application.
+   *
+   * @param args The command line arguments.
+   */
+  public static void main(final String[] args) {
+    SpringApplication.run(ECommerceApplication.class, args);
+  }
 
 }
+// CHECKSTYLE:ON: AbbreviationAsWordInName
